c11f86faca69d58a82350440297597e2e87039f3
KHromov_Adam <409806@niuitmo.ru>
2024-05-04 06:50:08 +0300

    vector отформатированный

--- src/vector.h
+++ src/vector.h
@@ -8,16 +8,16 @@
 #include <new>
 #include <utility>
 
-template <typename T> class vector {
-
+template <typename T>
+class vector {
 public:
   using value_type = T;
 
-  using reference = T &;
-  using const_reference = const T &;
+  using reference = T&;
+  using const_reference = const T&;
 
-  using pointer = T *;
-  using const_pointer = const T *;
+  using pointer = T*;
+  using const_pointer = const T*;
 
   using iterator = pointer;
   using const_iterator = const_pointer;
@@ -29,12 +29,10 @@ private:
 
 private:
   void reverse_destroy_n(iterator begin, std::size_t n) noexcept {
-    std::destroy(std::reverse_iterator<iterator>(begin + n),
-                 std::reverse_iterator<iterator>(begin));
+    std::destroy(std::reverse_iterator<iterator>(begin + n), std::reverse_iterator<iterator>(begin));
   }
 
-  void uninitialized_copy_n_with_reverse_destroy(iterator dest, iterator from,
-                                                 std::size_t n) {
+  void uninitialized_copy_n_with_reverse_destroy(iterator dest, iterator from, std::size_t n) {
     iterator cur = dest;
     for (std::size_t i = 0; i < n; ++i, ++from, ++cur) {
       try {
@@ -47,8 +45,7 @@ private:
   }
 
   pointer reallocate(std::size_t new_capacity) {
-    auto new_data =
-        static_cast<pointer>(operator new(new_capacity * sizeof(value_type)));
+    auto new_data = static_cast<pointer>(operator new(new_capacity * sizeof(value_type)));
     try {
       uninitialized_copy_n_with_reverse_destroy(new_data, data_, size_);
     } catch (...) {
@@ -65,28 +62,28 @@ private:
     capacity_ = new_capacity;
   }
 
-  iterator no_const(const_iterator it) { return begin() + (it - begin()); }
+  iterator no_const(const_iterator it) {
+    return begin() + (it - begin());
+  }
 
 public:
   // O(1) nothrow
   vector() noexcept = default;
 
   // O(1) nothrow
-  void swap(vector &other) noexcept {
+  void swap(vector& other) noexcept {
     std::swap(data_, other.data_);
     std::swap(size_, other.size_);
     std::swap(capacity_, other.capacity_);
   }
 
   // O(N) strong
-  vector(const vector &other)
-      : size_(other.size_), capacity_(size_),
-        data_(other.capacity_ ? static_cast<pointer>(operator new(
-                                    other.size_ * sizeof(value_type)))
-                              : nullptr) {
+  vector(const vector& other)
+      : size_(other.size_)
+      , capacity_(size_)
+      , data_(other.capacity_ ? static_cast<pointer>(operator new(other.size_ * sizeof(value_type))) : nullptr) {
     try {
-      uninitialized_copy_n_with_reverse_destroy(data_, other.data_,
-                                                other.size_);
+      uninitialized_copy_n_with_reverse_destroy(data_, other.data_, other.size_);
     } catch (...) {
       operator delete(data_);
       throw;
@@ -94,14 +91,16 @@ public:
   }
 
   // O(1) strong
-  vector(vector &&other)
-      : size_(other.size_), capacity_(other.capacity_), data_(other.data_) {
+  vector(vector&& other)
+      : size_(other.size_)
+      , capacity_(other.capacity_)
+      , data_(other.data_) {
     other.data_ = nullptr;
     other.size_ = 0;
   }
 
   // O(N) strong
-  vector &operator=(const vector &other) {
+  vector& operator=(const vector& other) {
     if (this != &other) {
       vector copy(other);
       swap(copy);
@@ -110,7 +109,7 @@ public:
   }
 
   // O(1) strong
-  vector &operator=(vector &&other) {
+  vector& operator=(vector&& other) {
     if (this != &other) {
       swap(other);
     }
@@ -124,34 +123,52 @@ public:
   }
 
   // O(1) nothrow
-  reference operator[](size_t index) { return data_[index]; }
+  reference operator[](size_t index) {
+    return data_[index];
+  }
 
   // O(1) nothrow
-  const_reference operator[](size_t index) const { return data_[index]; }
+  const_reference operator[](size_t index) const {
+    return data_[index];
+  }
 
   // O(1) nothrow
-  pointer data() noexcept { return data_; }
+  pointer data() noexcept {
+    return data_;
+  }
 
   // O(1) nothrow
-  const_pointer data() const noexcept { return data_; }
+  const_pointer data() const noexcept {
+    return data_;
+  }
 
   // O(1) nothrow
-  size_t size() const noexcept { return size_; }
+  size_t size() const noexcept {
+    return size_;
+  }
 
   // O(1) nothrow
-  reference front() { return *data_; }
+  reference front() {
+    return *data_;
+  }
 
   // O(1) nothrow
-  const_reference front() const { return *data_; }
+  const_reference front() const {
+    return *data_;
+  }
 
   // O(1) nothrow
-  reference back() { return data_[size_ - 1]; }
+  reference back() {
+    return data_[size_ - 1];
+  }
 
   // O(1) nothrow
-  const_reference back() const { return data_[size_ - 1]; }
+  const_reference back() const {
+    return data_[size_ - 1];
+  }
 
   // O(1)* strong
-  void push_back(const T &element) {
+  void push_back(const T& element) {
     if (size_ == capacity_) {
       std::size_t new_capacity = capacity_ * 2 + 1;
       pointer new_data = reallocate(new_capacity);
@@ -173,13 +190,19 @@ public:
   }
 
   // O(1) nothrow
-  void pop_back() { data_[--size_].~value_type(); }
+  void pop_back() {
+    data_[--size_].~value_type();
+  }
 
   // O(1) nothrow
-  bool empty() const noexcept { return size() == 0; }
+  bool empty() const noexcept {
+    return size() == 0;
+  }
 
   // O(1) nothrow
-  size_t capacity() const noexcept { return capacity_; }
+  size_t capacity() const noexcept {
+    return capacity_;
+  }
 
   // O(N) strong
   void reserve(size_t new_capacity) {
@@ -205,19 +228,27 @@ public:
   }
 
   // O(1) nothrow
-  iterator begin() noexcept { return data_; }
+  iterator begin() noexcept {
+    return data_;
+  }
 
   // O(1) nothrow
-  iterator end() noexcept { return data_ + size_; }
+  iterator end() noexcept {
+    return data_ + size_;
+  }
 
   // O(1) nothrow
-  const_iterator begin() const noexcept { return data_; }
+  const_iterator begin() const noexcept {
+    return data_;
+  }
 
   // O(1) nothrow
-  const_iterator end() const noexcept { return data_ + size_; }
+  const_iterator end() const noexcept {
+    return data_ + size_;
+  }
 
   // O(N) strong (swap)
-  iterator insert(const_iterator pos, const T &value) {
+  iterator insert(const_iterator pos, const T& value) {
     std::size_t index = pos - begin();
     push_back(value);
     iterator ret_it = begin() + index;
@@ -228,12 +259,13 @@ public:
   }
 
   // O(N) nothrow(swap)
-  iterator erase(const_iterator pos) { return erase(pos, std::next(pos)); }
+  iterator erase(const_iterator pos) {
+    return erase(pos, std::next(pos));
+  }
 
   // O(N) nothrow(swap)
   iterator erase(const_iterator first, const_iterator last) {
-    iterator ret_it = no_const(first), last_ = no_const(last),
-             first_ = no_const(first);
+    iterator ret_it = no_const(first), last_ = no_const(last), first_ = no_const(first);
     while (last_ != end()) {
       std::swap(*(first_++), *(last_++));
     }
